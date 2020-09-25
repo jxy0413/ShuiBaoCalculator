6ef46cc822140588a94c461dc5d9b483abc1ef80
cbeann <cbeann@163.com>
2020-09-25 20:12:28 +0800

    add cors config

---
+++ src/main/java/cn/bjfu/calculator/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package cn.bjfu.calculator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author chaird
+ * @create 2020-09-25 20:11
+ */
+@Configuration
+
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+
+
+    @Override
+
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+
+                .allowedOrigins("*")
+
+                .allowCredentials(true)
+
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+
+                .maxAge(3600);
+
+    }
+
+
+
+}
